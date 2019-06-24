56ed5e9a3f85da6d72c0c1e49553601b0d837165
Leo <leosc86@gmail.com>
2019-06-23 23:43:25 -0300

    agregada imagen de fondo HOME

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Background from "../images/Background.gif";
 
 class Home extends React.Component {
   render() {
@@ -6,7 +7,7 @@ class Home extends React.Component {
       <React.Fragment>
         <div class="text-center">
           <h3 className="mb-4 mt-5">Welcome to my Rick and Morty website</h3>
-          <img src="https://media.giphy.com/media/q9KaEjVuKcpl6/giphy.gif" />
+          <img src={Background} />
         </div>
       </React.Fragment>
     );
