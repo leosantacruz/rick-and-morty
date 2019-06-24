8f3f9f7c3699a67220a9717c3f0df1a87959f850
Leo <leosc86@gmail.com>
2019-06-23 21:57:58 -0300

    Updates

---
+++ precache-manifest.8cbfba1900cb4103dac26e4cfc7badb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cd08d083176ef97a490f6bee3e3a4f9",
+    "url": "/rick-and-morty/index.html"
+  },
+  {
+    "revision": "b88c985b5c855ac55f4e",
+    "url": "/rick-and-morty/static/css/2.43d354e1.chunk.css"
+  },
+  {
+    "revision": "40b3a96a5b3d1227f99c",
+    "url": "/rick-and-morty/static/css/main.8e9b02b2.chunk.css"
+  },
+  {
+    "revision": "b88c985b5c855ac55f4e",
+    "url": "/rick-and-morty/static/js/2.12a5e827.chunk.js"
+  },
+  {
+    "revision": "40b3a96a5b3d1227f99c",
+    "url": "/rick-and-morty/static/js/main.b405edb9.chunk.js"
+  },
+  {
+    "revision": "c578be119491bc2bef3f",
+    "url": "/rick-and-morty/static/js/runtime~main.6390f2d1.js"
+  }
+]);
